e8732bf94488acf3c353a6e2ea3de63d25184ca8
maxgren6-afk <maxgren6@gmail.com>
2026-09-16 17:43:50 +0200

    Added Dash + Wall Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.Hierarchy;
+
 // using System.Runtime.CompilerServices;
 // using UnityEditor.UIElements;
 using UnityEngine;
@@ -8,18 +10,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputActionReference move;
     [SerializeField] private InputActionReference jump;
+    [SerializeField] private InputActionReference dash;
     private float moveDirection;
 
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float jumpForce = 200f;
-    [SerializeField] private Transform leftFoot, rightFoot;
-    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform leftFoot, rightFoot, leftArm, rightArm;
+    [SerializeField] private LayerMask whatIsGround, whatIsWall;
     [SerializeField] private float raycastDistance = 0.25f;
     [SerializeField] private AudioClip jumpSoundEffect;
-    [SerializeField] private ParticleSystem jumpParticleSystem;
+    [SerializeField] private ParticleSystem jumpParticleSystem, dashParticleSystem;
+    [SerializeField] private float wallJumpForceX, wallJumpForceY, wallJumpCooldown, dashForce, dashCooldown;
+ 
 
     bool canMove = true;
-
+    bool canWallJump = true;
+    bool canDash = true;
 
 
 
@@ -39,6 +45,7 @@ public class PlayerMovement : MonoBehaviour
 
         
         jump.action.started += Jump;
+        dash.action.started += Dash;
     }
 
 
@@ -74,6 +81,7 @@ public class PlayerMovement : MonoBehaviour
     private void OnDisable()
     {
         jump.action.started -= Jump;
+        dash.action.started -= Dash;
     }
 
     private void FlipSprite(bool direction)
@@ -84,14 +92,77 @@ public class PlayerMovement : MonoBehaviour
     // Metod för jump funktionen, context viktigt
     private void Jump(InputAction.CallbackContext context)
     {
+        RaycastHit2D leftHit = Physics2D.Raycast(leftArm.position, Vector2.left, raycastDistance, whatIsWall);
+        RaycastHit2D rightHit = Physics2D.Raycast(rightArm.position, Vector2.right, raycastDistance, whatIsWall);
+
+
         if (CheckIsGrounded() == true)
         {
             rgbd.AddForce(new Vector2(0, jumpForce));
             audioSource.PlayOneShot(jumpSoundEffect);
 
-            jumpParticleSystem.Play();
+            CallJumpParticle();
+        }
+
+        if (CheckIsWall() == true && !CheckIsGrounded())
+        {
+            if (leftHit.collider != null && leftHit && canWallJump)
+            {
+                //Resettar all velocity så att man inte kan chain hoppa på väggar
+                rgbd.linearVelocity = new Vector2(0, 0);
+
+                rgbd.AddForce(new Vector2(wallJumpForceX, wallJumpForceY));
+                audioSource.PlayOneShot(jumpSoundEffect);
+
+                CallJumpParticle();
+
+                CallWalljumpSpent();
+            }
+
+            if (rightHit.collider != null && rightHit && canWallJump)
+            {
+                //Resettar all velocity så att man inte kan chain hoppa på väggar
+                rgbd.linearVelocity = new Vector2(0, 0);
+
+                rgbd.AddForce(new Vector2(-wallJumpForceX, wallJumpForceY));
+                audioSource.PlayOneShot(jumpSoundEffect);
+
+                CallJumpParticle();
+
+                CallWalljumpSpent();
+            }
         }
         
+    }
+
+    private void Dash(InputAction.CallbackContext context)
+    {
+        if (canDash)
+        {
+            canMove = false;
+
+            rgbd.linearVelocity = new Vector2(0, 0);
+
+            if (!rend.flipX)
+            {
+                rgbd.AddForce(new Vector2(dashForce, 0));
+            }
+            else
+            {
+                rgbd.AddForce(new Vector2(-dashForce, 0));
+            }
+
+            //Gör en trigger som heter Dash om man vill ha en animation
+            //anim.SetTrigger("Dash");
+
+            CallDashParticle();
+
+            CallDashSpent();
+
+            Invoke("CanMoveAgain", 0.3f);
+
+
+        }
     }
 
     private bool CheckIsGrounded()
@@ -114,6 +185,21 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+    private bool CheckIsWall()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(leftArm.position, Vector2.left, raycastDistance, whatIsWall);
+        RaycastHit2D rightHit = Physics2D.Raycast(rightArm.position, Vector2.right, raycastDistance, whatIsWall);
+
+        if (leftHit.collider != null && leftHit || rightHit.collider != null && rightHit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public void TakeKnockback(float knockbackForce, float upwardsForce)
     {
         canMove = false;
@@ -124,5 +210,38 @@ public class PlayerMovement : MonoBehaviour
     private void CanMoveAgain()
     {
         canMove = true;
+    }
+
+    private void CallJumpParticle()
+    {
+        jumpParticleSystem.Play();
+    }
+
+    private void CallDashParticle()
+    {
+        dashParticleSystem.Play();
+    }
+
+    private void CallWalljumpSpent()
+    {
+        canWallJump = false;
+
+        Invoke(nameof(CallWallJumpReset), wallJumpCooldown);
+    }
+
+    private void CallWallJumpReset()
+    {
+        canWallJump = true;
+    }
+
+    private void CallDashSpent()
+    {
+        canDash = false;
+
+        Invoke("CallDashReset", dashCooldown);
+    }
+    private void CallDashReset()
+    {
+        canDash = true;
     }
 }

c2d041b6b727d57be50d2eb8b61d3a27d218419e
a <isaacbakall29@gmail.com>
2026-09-26 17:58:22 +0200

    new sprite boss

---
+++ Assets/Scripts/Boss/BossMovement.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private float bounciness = 100f;
+    [SerializeField] private int damageGiven = 1;
+    [SerializeField] private float knockbackForce = 100f;
+    [SerializeField] private float upwardsForce = 5f;
+    [SerializeField] private AudioClip enemyDestroy;
+
+
+
+    private SpriteRenderer rend;
+    private AudioSource audioSource;
+    private bool canTakeDamage = true;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (moveSpeed > 0)
+        {
+            rend.flipX = false;
+        }
+
+        if (moveSpeed < 0)
+        {
+            rend.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2(moveSpeed, 0) * Time.deltaTime);
+    }
+
+    public void ReverseDirection()
+    {
+        moveSpeed *= -1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Lägg till || om enemy collision vill has
+        if (other.gameObject.CompareTag("EnemyBlock") || other.gameObject.CompareTag("Enemy"))
+        {
+            moveSpeed = moveSpeed * -1;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageGiven);
+            PlayerMovement playerMovement = other.gameObject.GetComponent<PlayerMovement>();
+            BossOpossum boss = GetComponent<BossOpossum>();
+
+            if (other.transform.position.x > transform.position.x)
+            {
+                if (boss != null)
+                {
+                    playerMovement.TakeBossKnockback(knockbackForce, upwardsForce);
+                }
+                else
+                {
+                    playerMovement.TakeKnockback(knockbackForce, upwardsForce);
+                }
+            }
+            else
+            {
+                if (boss != null)
+                {
+                    playerMovement.TakeBossKnockback(-knockbackForce, upwardsForce);
+                }
+                else
+                {
+                    playerMovement.TakeKnockback(-knockbackForce, upwardsForce);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerSword"))
+        {
+            Rigidbody2D rgbd = other.attachedRigidbody;
+
+            if (rgbd != null)
+            {
+                rgbd.linearVelocity = new Vector2(rgbd.linearVelocity.x, 0);
+                rgbd.AddForce(new Vector2(0, bounciness));
+
+                // Instantiate(enemyDestroy, transform.position, Quaternion.identity);
+            }
+
+            BossHealth bossHealth = GetComponent<BossHealth>();
+
+            if (bossHealth != null && canTakeDamage)
+            {
+                bossHealth.TakeDamage(1);
+
+                canTakeDamage = false;
+
+                Invoke(nameof(CallBossTakeDamageReset), 0.1f);
+            }
+        }
+    }
+
+    private void CallBossTakeDamageReset()
+    {
+        canTakeDamage = true;
+    }
+}
--- Assets/Scripts/Checkpoint_Bossrespawn.cs
+++ Assets/Scripts/Checkpoint_Bossrespawn.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class Checkpoint_Bossrespawn : MonoBehaviour
 {
     [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject bossbar;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             boss.SetActive(true);
+            bossbar.SetActive(true);
         }
 
-        if (boss != null)
+        /*if (boss != null)
         {
             GetComponent<BossRespawn>().RespawnBoss();
-        }
+        }*/
     }
 }
